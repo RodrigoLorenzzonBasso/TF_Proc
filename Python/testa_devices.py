cb9b849306e15c38740a4518aaca00039356845a
RodrigoLorenzzon <rodrigolorenzzonbasso@gmail.com>
2019-11-08 21:49:31 -0200

    Bluetooth

--- Python/testa_devices.py
+++ Python/testa_devices.py
@@ -1,4 +1,5 @@
 import bluetooth
+import subprocess
 
 def main():
 	print("Hello World!")
@@ -6,15 +7,24 @@ def main():
 	target_name = "HC-06"
 	target_address = None
 	port = 32123
-	data_size = 1024
+	data_size = 20
+	passkey = "1234"
+
+	subprocess.call("kill -9 `pidof bluetooth-agent`",shell=True)
+
+	status = subprocess.call("bluetooth-agent " + passkey + " &",shell=True)
 
 	nearby_devices = bluetooth.discover_devices(lookup_names=True)
 	print("found %d devices" % len(nearby_devices))
 
-	for bdaddr in nearby_devices:
-		if target_name == bluetooth.lookup_name(bdaddr):
-			target_address = bdaddr
+	print(nearby_devices)
+
+	for device in nearby_devices:
+		if target_name == bluetooth.lookup_name(device[0]):
+			target_address = device[0]
 			break
+	
+	target_address = '98:D3:31:F5:0C:5E'
 
 	if target_address is not None:
 		sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -24,6 +34,7 @@ def main():
 			data = sock.recv(data_size)
 			if data:
 				print(data.decode("utf-8"))
+				data = 0
 	else:
 		print("No device found")
 
