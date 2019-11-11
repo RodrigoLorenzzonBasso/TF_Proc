0af3ea1700b168a0a1b5501e45cced04f580be46
rodrigolorenzzon <rodrigo.l.basso@pucrs.br>
2019-11-11 10:27:30 -0300

    Bluetooth scripts

---
+++ Python/teste.py
@@ -0,0 +1,28 @@
+
+data = "423 123 459 s ff"
+
+splitted = data.split(" ")
+
+print(data)
+print(splitted)
+
+with open ("output.csv","w"):
+    pass
+
+for i in range(5):
+    with open("output.csv","a") as f:
+        for word in splitted:
+            f.write(word)
+            if word != splitted[-1]:
+                f.write(',')
+        f.write("\n")
+
+
+b_data = b'asdsasdas\n'
+
+print(b_data)
+print(len(b_data))
+decode = b_data.decode("utf-8")
+print(decode)
+print(len(decode))
+print(type(decode))
